6d591ca32f484f8c8391588062c06464edc3675f
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-08-19 19:43:50 +0930

    Updated run_weather_app

--- R/run_weather_app.R
+++ R/run_weather_app.R
@@ -2,22 +2,29 @@
 #'
 #' Runs a shiny app where a user can select and download weather data.
 #'
-#' @return A list containing ggplot2 objects which are diagnostic plots.
-#'
-#' @param browser A logical flag to run app directly in the browser
-#'
-#' @importFrom shiny runApp
-#'
 #' @examples
 #' \dontrun{
 #' run_weather_app()
 #' }
 #' @export
-run_weather_app <- function(browser = FALSE) {
+run_weather_app <- function() {
   appDir <- system.file("weather_app", package = "weathervane")
-  if (appDir == "") {
-    stop("Could not find weather_app Try re-installing `weathervane`.", call. = FALSE)
+  if (!requireNamespace("shiny", quietly = TRUE)) {
+    stop("Package \"shiny\" is needed for this function to work. Please install it.",
+         call. = FALSE)
   }
-
-  shiny::runApp(appDir, display.mode = "normal")
+  else if(!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop("Package \"ggplot2\" is needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  else if(!requireNamespace("leaflet", quietly = TRUE)) {
+    stop("Package \"leaflet\" is needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  else if (appDir == "") {
+    stop("Could not find weather_app. Try re-installing `weathervane`.", call. = FALSE)
+  }
+  else {
+    shiny::runApp(appDir, display.mode = "normal")
+  }
 }
