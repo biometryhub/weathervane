2052d9104cadb2c8934209d6e2ff748767b81408
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-11-18 14:24:48 +1030

    Refactored the running app file

--- R/run_weather_app.R
+++ R/run_weather_app.R
@@ -10,19 +10,13 @@
 run_weather_app <- function() {
   appDir <- system.file("weather_app", package = "weathervane")
 
-  if (!requireNamespace("shiny", quietly = TRUE)) {
-    stop("Package \"shiny\" is needed for this function to work. Please install it.",
-         call. = FALSE)
+  installed_pkgs <- rownames(installed.packages())
+  not_found <- setdiff(c("ggplot2", "leaflet", "shiny"), grep("(^ggplot2$)|(^shiny$)|(^leaflet$)", installed_pkgs, value = T))
+  if (length(not_found) > 0) {
+    stop("Package ", not_found[1], " is needed for this function to work. Please install it.", call. = FALSE)
   }
-  else if(!requireNamespace("ggplot2", quietly = TRUE)) {
-    stop("Package \"ggplot2\" is needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
-  else if(!requireNamespace("leaflet", quietly = TRUE)) {
-    stop("Package \"leaflet\" is needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
-  else if (appDir == "") {
+
+  if (appDir == "") {
     stop("Could not find weather_app. Try re-installing `weathervane`.", call. = FALSE)
   }
   else {
